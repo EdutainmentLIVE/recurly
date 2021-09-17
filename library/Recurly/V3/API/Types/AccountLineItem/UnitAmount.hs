a6705d35b993507df64e577bd74d7c867641f200
Andres Schmois <andres@itpro.tv>
2021-09-17 11:17:09 -0400

    Fix comment

--- library/Recurly/V3/API/Types/AccountLineItem/UnitAmount.hs
+++ library/Recurly/V3/API/Types/AccountLineItem/UnitAmount.hs
@@ -5,7 +5,7 @@ import Recurlude
 import qualified Recurly.V3.API.Types.Money.Money as Money
 
 -- Note: This value can be negative or positive and recurly does not care, its value
--- is governed by AccountLineItem.Type.
+-- is governed by Recurly.V3.API.Types.AccountLineItem.Type.
 -- A positive or negative amount with type=charge will result in a positive unit_amount.
 -- A positive or negative amount with type=credit will result in a negative unit_amount.
 -- https://developers.recurly.com/api/v2019-10-10/index.html#operation/create_line_item
