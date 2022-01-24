f582be6c31d284aa69984c6399f6289daa11c310
Sara <sara@itpro.tv>
2022-01-24 13:24:48 -0700

    [sc-104787] Add year

--- library/Recurly/V3/API/Types/BillingInfo/PaymentMethod.hs
+++ library/Recurly/V3/API/Types/BillingInfo/PaymentMethod.hs
@@ -14,6 +14,7 @@ data PaymentMethod = PaymentMethod
   , cardType :: PaymentMethod.CardType
   , lastFour :: Text
   , month :: Integer
+  , year :: Integer
   , payPalBillingAgreementId :: Maybe Text
   }
   deriving (Eq, Show)
@@ -24,5 +25,6 @@ instance FromJSON PaymentMethod where
     cardType <- aesonRequired obj "card_type"
     lastFour <- aesonRequired obj "last_four"
     month <- aesonRequired obj "exp_month"
+    year <- aesonRequired obj "exp_year"
     payPalBillingAgreementId <- aesonOptional obj "billing_agreement_id"
-    pure PaymentMethod { object, cardType, lastFour, month, payPalBillingAgreementId }
+    pure PaymentMethod { object, cardType, lastFour, month, year, payPalBillingAgreementId }
